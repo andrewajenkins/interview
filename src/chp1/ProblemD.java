af666e22345259c3eb2643960539bd1b0050c0fc
Andrew Jenkins <andrewajenkins@outlook.com>
2017-06-06 11:03:56 -0700

    add problem 1.4

---
+++ src/chp1/ProblemD.java
@@ -0,0 +1,24 @@
+package chp1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemD {
+
+    @Test
+    public void test() {
+        String word1 = "abcdefgc";
+        String word2 = "defgabcc";
+        int[] counts = new int[256];
+        for(char c : word1.toCharArray()) {
+            counts[c]++;
+        }
+        for(char c : word2.toCharArray()) {
+            counts[c]--;
+        }
+        for(int i = 0; i < counts.length; i++) {
+            if(counts[i] != 0) fail(); 
+        }
+    }
+}
