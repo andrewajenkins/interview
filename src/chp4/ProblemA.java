b95f822a10b8e33cd141bcd7cf7349f7fa3b08ee
Andrew Jenkins <andrewajenkins@outlook.com>
2017-06-08 12:44:52 -0700

    add problem 4.1

---
+++ src/chp4/ProblemA.java
@@ -0,0 +1,70 @@
+package chp4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 4.1: Implement a function to check if a tree is balanced. For the purposes of this question,
+ * a balanced tree is defined to be a tree such that no two leaf nodes differ in distance from
+ * the root by more than one node.
+ */
+public class ProblemA {
+
+    private static final int MAX_DEPTH = 3;
+
+    @Test
+    public void test() {
+        Node rootBalanced = buildBalancedTree(new Node(0), 1);
+        assertTrue(isBalanced(rootBalanced));
+        Node rootLinear = buildLinearTree(new Node(0), 1);
+        assertFalse(isBalanced(rootLinear));
+    }
+
+    boolean isBalanced(Node root) {
+        int max = getMax(root, 0);
+        int min = getMin(root, 0);
+        return (max - min) <= 1;
+    }
+
+    private int getMin(Node root, int count) {
+        count++;
+        if(root.left == null || root.right == null) return count;
+        return Math.min(getMin(root.left, count), getMin(root.right, count));
+    }
+
+    private int getMax(Node root, int count) {
+        count++;
+        if(root.left == null && root.right == null) return count;
+        int left = Integer.MAX_VALUE;
+        if(root.left != null) left = getMax(root.left, count);
+        int right = Integer.MAX_VALUE;
+        if(root.right != null) right = getMax(root.right, count);
+        return Math.max(left, right);
+    }
+
+    class Node {
+        Node left;
+        Node right;
+        int data;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node buildBalancedTree(Node current, int i) {
+        if(i > MAX_DEPTH) return current;
+        current.left = new Node(i++);
+        current.right = new Node(i++);
+        buildBalancedTree(current.left, i);
+        buildBalancedTree(current.right, i);
+        return current;
+    }
+
+    Node buildLinearTree(Node current, int i) {
+        if(i > MAX_DEPTH) return current;
+        current.right = new Node(i++);
+        buildLinearTree(current.right, i);
+        return current;
+    }
+}
