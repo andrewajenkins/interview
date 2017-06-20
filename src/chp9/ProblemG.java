a58a284903304565bd534baaea3a64f94a8e038a
Andrew Jenkins <andrewajenkins@outlook.com>
2017-06-20 12:23:42 -0700

    add problem 9.7 part 2

--- src/chp9/ProblemG.java
+++ src/chp9/ProblemG.java
@@ -1,6 +1,9 @@
 package chp9;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 import org.junit.Test;
 
@@ -17,5 +20,26 @@ public class ProblemG {
     public void test() {
         int[][] input = {{65,100},{70,150},{56,90},{75,190},{60,95},{68,110}};
         int[][] output = {{56,90},{60,95},{65,100},{68,110},{70,150},{75,190}};
+        assertTrue(Arrays.equals(maxSequence(input), output));
+    }
+
+    public int[][] maxSequence(int[][] a) {
+        Arrays.sort(a, new HeightComparator());
+        Arrays.sort(a, new WeightComparator());
+        return a;
+    }
+
+    public class HeightComparator implements Comparator<Integer[]> {
+        @Override
+        public int compare(Integer[] a, Integer[] b) {
+            return a[0].compareTo(b[0]);
+        }
+    }
+
+    public class WeightComparator implements Comparator<Integer[]> {
+        @Override
+        public int compare(Integer[] a, Integer[] b) {
+            return a[1].compareTo(b[1]);
+        }
     }
 }
